bfca7471300fba5e82e985f568ca6e8387757364
moussetc <moussetc@users.noreply.github.com>
2019-05-13 20:14:36 +0200

    Configure Babel and eslint-plugin-react to avoid build warnings

--- webapp/babel.config.js
+++ webapp/babel.config.js
@@ -9,6 +9,7 @@ module.exports = {
                 safari: 12,
             },
             useBuiltIns: 'usage',
+            corejs: '2.0.0',
             shippedProposals: true,
         }],
         '@babel/preset-react',

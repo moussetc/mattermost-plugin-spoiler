c778f27076a58cdb263716d1fd0d45bdd5ca17ce
moussetc <moussetc@users.noreply.github.com>
2025-05-04 19:35:27 +0200

    Redacted mode: make spoiler content accessible
    This requires a complete rework of the render, instead of using fake
    text character, we display the correct elements but hide them with CSS,
    and add aria attributes to allow screen readers to access the content.

--- webapp/src/components/spoiler_post_type/spoiler_post_type.jsx
+++ webapp/src/components/spoiler_post_type/spoiler_post_type.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './spoiler_post_type.scss';
 
 const {formatText, messageHtmlToComponent} = window.PostUtils;
 
@@ -16,48 +17,45 @@ export default class SpoilerPostType extends React.PureComponent {
             displaySpoilerContent: false,
         };
 
-        this.revealSpoiler = () => {
+        this.revealSpoiler = (e) => {
+            e.preventDefault();
             if (!this.state.displaySpoilerContent) {
                 this.setState({displaySpoilerContent: true});
             }
         };
-
-        this.renderNormal = (formattedSpoilerCaption, spoilerContent) => {
-            const formattedSpoilerContent = messageHtmlToComponent(formatText(spoilerContent));
-            return (
-                <div>{formattedSpoilerCaption ? <div>{formattedSpoilerCaption}</div> : ''} {formattedSpoilerContent}</div>
-            );
-        };
-
-        this.renderSpoiler = (formattedSpoilerCaption, spoilerContent) => {
-            // don't display real text so emoji, url, image... are not visible
-            const yaourt = Array.from(spoilerContent).
-                map((c) => ((/\s/).test(c) ? c : '_')).join('');
-            const lines = yaourt.split(/\r?\n/).map((line) => messageHtmlToComponent(line));
-            const divProps = {
-                onClick: this.revealSpoiler,
-                style: {background: this.props.theme.centerChannelColor},
-                title: 'Reveal spoiler',
-            };
-            return (
-                <div>
-                    {formattedSpoilerCaption ? <div>{formattedSpoilerCaption}</div> : ''}
-                    {lines.map((line, index) => {
-                        return <div key={index}><span {...divProps}>{line}<br/></span></div>;
-                    })}
-                </div>
-            );
-        };
     }
 
     render() {
-        // Don't use post.message directly as it has a special formatting used by the native apps
         const post = {...this.props.post};
-        const spoilerCaption = messageHtmlToComponent(formatText(post.message || ''));
+        const spoilerCaption = post.message || '';
         const spoilerContent = post.props.CustomSpoilerRawMessage || '';
-        if (this.state.displaySpoilerContent) {
-            return this.renderNormal(spoilerCaption, spoilerContent);
-        }
-        return this.renderSpoiler(spoilerCaption, spoilerContent);
+        const showSpoiler = this.state.displaySpoilerContent;
+
+        const formattedCaption = messageHtmlToComponent(formatText(spoilerCaption));
+        const formattedContent = messageHtmlToComponent(formatText(spoilerContent));
+
+        const divProps = {
+            onClick: this.revealSpoiler,
+        };
+        return (
+            <div>
+                {formattedCaption ? <div>{formattedCaption}</div> : ''}
+                <span
+                    className={`spoiler-content ${showSpoiler ? 'reveal-spoiler' : ''}`}
+                    aria-label='Show spoiler'
+                    aria-expanded={showSpoiler}
+                    tabIndex='0'
+                    role='button'
+                    {...divProps}
+                >
+                    <span
+                        aria-hidden={!showSpoiler}
+                        className='spoilered-text-content'
+                    >
+                        {formattedContent}
+                    </span>
+                </span>
+            </div>
+        );
     }
 }
